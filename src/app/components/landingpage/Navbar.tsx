069699a09c68eed6fc4850db0445dc694a7fa121
Arjun Singh <arjunsingh23312331@gmail.com>
2025-03-15 02:03:58 +0530

    pulling main

--- src/app/components/landingpage/Navbar.tsx
+++ src/app/components/landingpage/Navbar.tsx
@@ -3,6 +3,8 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { Menu, X, Moon, Sun } from 'lucide-react';
+import { RegisterLink,LoginLink } from '@kinde-oss/kinde-auth-nextjs';
+
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -88,18 +90,20 @@ const Navbar: React.FC = () => {
               >
                 {isDarkMode ? <Sun className="w-5 h-5" /> : <Moon className="w-5 h-5" />}
               </button>
-              <Link 
-                href="/login" 
+
+              <LoginLink postLoginRedirectURL="/dashboard"
+                
                 className="px-4 py-2 border border-indigo-600 text-indigo-600 dark:text-indigo-400 dark:border-indigo-400 font-medium rounded-lg hover:bg-indigo-50 dark:hover:bg-indigo-900/30 transition duration-150"
               >
-                Log In
-              </Link>
-              <Link 
-                href="/signup" 
+                Sign in
+              </LoginLink>
+
+              <RegisterLink 
+                 
                 className="px-4 py-2 bg-indigo-600 text-white font-medium rounded-lg hover:bg-indigo-700 transition duration-150"
               >
-                Sign Up
-              </Link>
+                Sign up
+              </RegisterLink>
             </div>
           </div>
           
@@ -154,20 +158,19 @@ const Navbar: React.FC = () => {
             FAQ
           </Link>
           <div className="flex space-x-2 px-3 py-2">
-            <Link 
-              href="/login" 
+            <LoginLink postLoginRedirectURL="/dashboard"  
               className="flex-1 px-4 py-2 text-center border border-indigo-600 text-indigo-600 dark:text-indigo-400 dark:border-indigo-400 font-medium rounded-lg"
               onClick={() => setIsOpen(false)}
             >
-              Log In
-            </Link>
-            <Link 
-              href="/signup" 
+              Sign in
+            </LoginLink>
+            <RegisterLink
+              
               className="flex-1 px-4 py-2 text-center bg-indigo-600 text-white font-medium rounded-lg"
               onClick={() => setIsOpen(false)}
             >
-              Sign Up
-            </Link>
+              Sign up
+            </RegisterLink>
           </div>
         </div>
       </div>
