4c79bd385de5d21db24f3e4408e272e4444e5dba
misterraina <ayushraina617@gmail.com>
2025-05-02 21:45:55 +0530

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://subspliter.com', // replace with your deployed site URL
+    generateRobotsTxt: true, // (optional)
+    sitemapSize: 5000,
+  };
+  
